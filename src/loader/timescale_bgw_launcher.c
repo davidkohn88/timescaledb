d4f323fe888c005775b8378ce7685a7c14ccf23f
David Kohn <david@timescale.com>
2018-05-15 11:23:02 -0400

    BGWs connecting to dbs.

--- src/loader/timescale_bgw_launcher.c
+++ src/loader/timescale_bgw_launcher.c
@@ -36,10 +36,10 @@
 #include <access/xact.h>
 #include <access/heapam.h>
 #include <access/htup_details.h>
+#include <utils/snapmgr.h>
 
-/* needed for initializing shared memory */
+/* needed for initializing shared memory and using various locks */
 #include <utils/hsearch.h>
-
 #include <storage/spin.h>
 
 #include "timescale_bgw_launcher.h"
@@ -133,10 +133,9 @@ static void timescale_bgw_shmem_startup(void){
         prev_shmem_startup_hook();
 
 
-    /* reset in case this i a restart within the postmaster */
+    /* reset in case this is a restart within the postmaster */
     tsbgw_ss = NULL;
     
-
     LWLockAcquire(AddinShmemInitLock, LW_EXCLUSIVE);
 
     tsbgw_ss = ShmemInitStruct(TSBGW_SS_NAME, sizeof(tsbgw_shared_state), &found);
@@ -159,18 +158,58 @@ static void timescale_bgw_shmem_startup(void){
     
 }
 
-/* Model this on autovacuum.c -> get_database_list */
+/* 
+ * Model this on autovacuum.c -> get_database_list
+ * Note that we are not doing all the things around memory context that they do, because 
+ * a) we're using shared memory to store the list of dbs and b) we're in a function and 
+ * shorter lived context here. 
+ */
 static void populate_database_htab(void){
-    List            *dblist = NIL;
     Relation        rel;
     HeapScanDesc    scan;
     HeapTuple       tup;
-    MemoryContext   resultcxt;
-
-    /* don't want output in our transaction context*/
-    resultcxt = CurrentMemoryContext;
+    
+    
     /* by this time we should already be connected to the db, and only have access to shared catalogs*/
     /*start a txn, see note in autovacuum.c for why*/
+    StartTransactionCommand();
+    (void) GetTransactionSnapshot();
+
+    rel = heap_open(DatabaseRelationId, AccessShareLock);
+    scan = heap_beginscan_catalog(rel, 0, NULL);
+
+    while (HeapTupleIsValid(tup = heap_getnext(scan, ForwardScanDirection)))
+    {
+        Form_pg_database    pgdb = (Form_pg_database) GETSTRUCT(tup);
+        tsbgw_hash_entry    *tsbgw_he;
+        Oid                 db_oid;
+        bool                hash_found;
+        
+
+        if (!pgdb->datallowconn) 
+            continue; /* don't bother with dbs that don't allow connections, we'll fail when starting a worker anyway*/
+        
+        db_oid = HeapTupleGetOid(tup);
+        if (hash_get_num_entries(tsbgw_ss->hashtable) >= TSBGW_MAX_DBS)
+            ereport(FATAL, (errmsg("More databases in cluster than allocated in shared memory, stopping cluster launcher")));
+            
+        /*acquire lock so we can access hash table*/
+        LWLockAcquire(tsbgw_ss->lock, LW_EXCLUSIVE);
+
+        tsbgw_he = (tsbgw_hash_entry *) hash_search(tsbgw_ss->hashtable, &db_oid, HASH_ENTER, &hash_found);
+        if (!hash_found)
+            tsbgw_he->ts_installed = FALSE;
+            snprintf(tsbgw_he->ts_version, NAMEDATALEN, "");
+            tsbgw_he->db_launcher_pid = 0;
+            tsbgw_he->num_active_workers = 0; 
+
+        LWLockRelease(tsbgw_ss->lock);
+    }
+
+    heap_endscan(scan);
+    heap_close(rel, AccessShareLock);
+    CommitTransactionCommand();
+
 
 }
 static void increment_total_workers(void)  {
@@ -197,49 +236,74 @@ static int get_total_workers(void){
 extern void timescale_bgw_launcher_main(void) {
     
     BackgroundWorker        worker;
-    BackgroundWorkerHandle  *handle;
-    BgwHandleStatus         status;
-    pid_t                   worker_pid;
-
-
-    Oid db_id = 13267; /*postgres db oid*/
+    HASH_SEQ_STATUS         hash_seq;
+    tsbgw_hash_entry        *current_entry;
 
 
     /* Establish signal handlers before unblocking signals. */
 	pqsignal(SIGHUP, timescale_bgw_sighup);
 	pqsignal(SIGTERM, timescale_bgw_sigterm);
     BackgroundWorkerUnblockSignals();
-    /* Connect to the db, no db name yet, so can only access shared catalogs*/
     increment_total_workers();  
     
+    /* Connect to the db, no db name yet, so can only access shared catalogs*/
     BackgroundWorkerInitializeConnection(NULL, NULL);
     ereport(LOG, (errmsg("Timescale BGW Launcher Connected To DB")));
+    populate_database_htab();
+    ereport(LOG, (errmsg("Database HTAB Populated")));
 
+    /*common parameters for all our scheduler workers*/
     memset(&worker, 0, sizeof(worker));
     worker.bgw_flags = BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION;
     worker.bgw_restart_time = BGW_NEVER_RESTART;
     worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
-    worker.bgw_notify_pid = MyProcPid;
     sprintf(worker.bgw_library_name, "timescaledb");
     sprintf(worker.bgw_function_name , "timescale_bgw_db_scheduler_main");
-    snprintf(worker.bgw_name, BGW_MAXLEN, "timescale_bgw_db_scheduler for db_id = %d", db_id);
-    worker.bgw_main_arg = db_id;
-
+    worker.bgw_notify_pid = MyProcPid;
     
-    RegisterDynamicBackgroundWorker(&worker, &handle);
-    status = WaitForBackgroundWorkerStartup(handle, &worker_pid);
-    ereport(LOG, (errmsg("Worker started with PID %d", worker_pid)));
+    /*now scan our hash table of dbs and register a worker for each*/
+    LWLockAcquire(tsbgw_ss->lock, AccessShareLock);
+    hash_seq_init(&hash_seq, tsbgw_ss->hashtable);
+    
+    while ((current_entry = hash_seq_search(&hash_seq)) != NULL)
+    {
+        BackgroundWorkerHandle  *handle;
+        BgwHandleStatus         status;
+        pid_t                   worker_pid;
+
+        ereport(LOG, (errmsg("On hash entry for db Oid = %d", current_entry->db_oid)));
+        worker.bgw_main_arg = current_entry->db_oid;
+        RegisterDynamicBackgroundWorker(&worker, &handle);
+        status = WaitForBackgroundWorkerStartup(handle, &worker_pid);
+        ereport(LOG, (errmsg("Worker started with PID %d", worker_pid)));
+    }
+    LWLockRelease(tsbgw_ss->lock);
+    decrement_total_workers();
+    ereport(LOG, (errmsg("Cluster Launcher shutting down")));
+
     
 }
 
 extern void timescale_bgw_db_scheduler_main(Oid db_id){
-    ereport(LOG, (errmsg("Timescale BGW DB Scheduler Started")));
-    increment_total_workers();
-    ereport(LOG, (errmsg("Total Workers = %d", get_total_workers())));
+    tsbgw_hash_entry        *hash_entry;
+    bool                    entry_found;
 
+    pqsignal(SIGHUP, timescale_bgw_sighup);
+	pqsignal(SIGTERM, timescale_bgw_sigterm);
+    BackgroundWorkerUnblockSignals();
+    ereport(LOG, (errmsg("Worker started for Database id = %d with pid %d", db_id, MyProcPid))); 
+    increment_total_workers();
     BackgroundWorkerInitializeConnectionByOid(db_id, NULL);
     ereport(LOG, (errmsg("Connected to Database id = %d", db_id)));
-
+    ereport(LOG, (errmsg("Total Workers = %d", get_total_workers())));
+    /*db scheduler can access shared memory and update pid with only a shared lock as there's only one worker per db*/
+    LWLockAcquire(tsbgw_ss->lock, AccessShareLock);
+    hash_entry = hash_search(tsbgw_ss->hashtable, &db_id, HASH_ENTER, &entry_found);
+    if (entry_found)
+    {
+        hash_entry->db_launcher_pid = MyProcPid;
+    }
+    LWLockRelease(tsbgw_ss->lock);
     decrement_total_workers();
 
 }
