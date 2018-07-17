2a6e051afcc6996aa7a718cd3ee9de6e33902393
David Kohn <david@timescale.com>
2018-07-17 17:16:26 -0400

    Compiling, still a problem with restart

---
+++ src/background_jobs.h
@@ -0,0 +1,7 @@
+#ifndef BACKGROUND_JOBS_H
+#define BACKGROUND_JOBS_H
+#include <postgres.h>
+#include <fmgr.h>
+
+PG_FUNCTION_INFO_V1(timescale_bgw_db_scheduler_main); 
+#endif /* BACKGROUND_JOBS_H*/
--- src/loader/timescale_bgw.h
+++ src/loader/timescale_bgw.h
@@ -8,9 +8,9 @@
 
 /* Interface for both the loader and the versioned extension*/
 
-extern bool timescale_bgw_increment_total_workers(int max_workers); 
-extern void timescale_bgw_decrement_total_workers(void);
-extern int timescale_bgw_get_total_workers(void);
+PG_FUNCTION_INFO_V1(timescale_bgw_increment_total_workers); 
+PG_FUNCTION_INFO_V1(timescale_bgw_decrement_total_workers);
+PG_FUNCTION_INFO_V1( timescale_bgw_get_total_workers);
 
 PG_FUNCTION_INFO_V1(timescale_bgw_restart_db_workers);
 
