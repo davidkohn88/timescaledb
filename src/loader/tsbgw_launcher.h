83f95d8e0460afc3c4ce528a4cb945027d216b4f
David Kohn <david@timescale.com>
2018-08-02 18:15:57 -0400

    Adding guc support

--- src/loader/tsbgw_launcher.h
+++ src/loader/tsbgw_launcher.h
@@ -4,12 +4,12 @@
 
 #include <postgres.h>
 
-
 extern void tsbgw_cluster_launcher_register(void);
 
 /*called by postmaster at launcher bgw startup*/
 extern void tsbgw_cluster_launcher_main(void);
 extern void tsbgw_db_scheduler_entrypoint(Oid db_id);
+extern void tsbgw_launcher_on_max_workers_guc_change(void);
 
 
 
